a10c9561c941b32eabe72efa8a7c7fcc060aeb32
Mahmutsefer <mahmutsefer94@gmail.com>
2026-09-01 14:50:01 +0000

    Update sitemap domain

--- website/app/sitemap.ts
+++ website/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = "https://emkurutemizleme.com";
+const baseUrl = "https://haseltemizlemesistemleri.com.tr";
 
 const services = [
   "kuru-temizleme",
